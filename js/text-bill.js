71bf20b272ef4207aa1a551a81459cc3c399bdbb
Viollah <Viollah0119@gmail.com>
2021-08-18 16:15:59 +0100

    second commit

--- js/text-bill.js
+++ js/text-bill.js
@@ -10,27 +10,6 @@ addToBillBtn.addEventListener('click', function() {
   textBill();
 });
 
-function textBill() {
-  var textB = billTypeText.value;
-  refFactory.billTotals(textB);
-  callTotalOne.innerHTML = refFactory.allCalls().toFixed(2);
-  smsTotalOne.innerHTML = refFactory.allSms().toFixed(2);
-  totalOne.innerHTML = refFactory.grandTotal().toFixed(2);
-
-  if (refFactory.grandTotal() >= 30) {
-    totalOne.classList.add("warning");
-  }
-  if (refFactory.grandTotal() >= 50) {
-    totalOne.classList.add("danger");
-  }
-
-  if (refFactory.grandTotal() < 20) {
-    totalOne.classList.remove("warning");
-  } else if (refFactory.grandTotal() < 50) {
-    totalOne.classList.remove("danger");
-
-  }
-}
 
 function Factory() {
     var call = 0;
